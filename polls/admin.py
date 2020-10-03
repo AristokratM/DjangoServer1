6066af05223d69811d797dab254ebd0f000f8933
AristokratM <61250102+AristokratM@users.noreply.github.com>
2020-10-04 01:14:04 +0300

    customise admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -9,6 +9,8 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Data Information', {'fields': ['pub_date']}),
